856a604b5ec77ef3b3f92b07b3f776cde2eb83c8
Julien H <nordinleo@yahoo.fr>
2020-03-17 20:31:56 +0100

    with route files

--- front/src/App.js
+++ front/src/App.js
@@ -1,14 +1,28 @@
 import React from 'react';
-import './App.css';
-import Nav from './components/nav';
+import Nav from './components/Nav';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+import Connect from './pages/Connect';
+import Help from './pages/Help';
+import BecomeAgent from './pages/BecomeAgent';
+import Home from './pages/Home';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-       <Nav /> 
-        
-      </header>
+      <Router>
+      <Nav />
+        <Switch>
+          <Route path="/connect" component={Connect} />         
+          <Route path="/help" component={Help} />
+          <Route path="/become-agent" component={BecomeAgent} />
+          <Route path="/" component={Home} />
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ front/src/pages/Help.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+function Help() {
+    return <h2>How can we help you?</h2>;
+}
+
+export default Help
+
