3f75f2812638244e0453d1ea9c9d117d527b07eb
Naïm <nassna85@hotmail.fr>
2020-03-19 15:59:06 +0100

    Seed for Roles Table

---
+++ back/seeders/20200319144846-RolesSeed.js
@@ -0,0 +1,26 @@
+"use strict";
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert(
+      "Roles",
+      [
+        {
+          name: "ROLE_USER",
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          name: "ROLE_AGENT",
+          createdAt: new Date(),
+          updatedAt: new Date()
+        }
+      ],
+      {}
+    );
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete("Roles", null, {});
+  }
+};
