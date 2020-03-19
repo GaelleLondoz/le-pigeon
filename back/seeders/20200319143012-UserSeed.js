5a36dfd83318391566753a1d9596fdc8846ed201
Naïm <nassna85@hotmail.fr>
2020-03-19 15:46:54 +0100

    Installation faker + Seed for Users Table

---
+++ back/seeders/20200319143012-UserSeed.js
@@ -0,0 +1,28 @@
+"use strict";
+
+const faker = require("faker");
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    let data = [];
+    let count = 30;
+
+    while (count--) {
+      data.push({
+        firstName: faker.name.firstName(),
+        lastName: faker.name.lastName(),
+        userName: faker.internet.userName(),
+        email: faker.internet.email(),
+        password: "password",
+        avatar: faker.image.avatar(),
+        createdAt: new Date(),
+        updatedAt: new Date()
+      });
+    }
+    return queryInterface.bulkInsert("Users", data, {});
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete("Users", null, {});
+  }
+};
