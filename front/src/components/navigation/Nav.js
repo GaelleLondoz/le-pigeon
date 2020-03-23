8eabc35954b5e12e3dee8dba6896abc36db92520
Julien H <nordinleo@yahoo.fr>
2020-03-23 10:49:59 +0100

    with colors and shapes

--- front/src/components/navigation/Nav.js
+++ front/src/components/navigation/Nav.js
@@ -1,11 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Link } from "react-router-dom";
 import RoutesNav from "./RoutesNav";
-/////////////////////////////////////////////
+//////////////// Material-ui /////////////////////////////
 import { makeStyles } from "@material-ui/core/styles";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
+/////////////////////////////////////////////////////////
+import "../../assets/sass/Nav.scss";
+import AccountCircleIcon from "@material-ui/icons/AccountCircle";
 
 // 2 // created navigation
 // export to App.js
@@ -21,7 +24,6 @@ const useStyles = makeStyles(theme => ({
   }
 }));
 
-
 function Nav() {
   const classes = useStyles();
 
@@ -30,13 +32,26 @@ function Nav() {
       <div className={classes.root}>
         <AppBar position="static" className={classes.root}>
           <Toolbar>
-
             <Typography variant="h5" className={classes.title}>
-              <Link>LE PIGEON</Link>
+              <Link to="/" className="navElement">
+                LE PIGEON
+              </Link>
             </Typography>
-            <Link to="/connect">Se connecter</Link>
-            <Link to="/help">Aide</Link>
-            <Link to="/become-agent">Devenez agent!</Link>
+            <Link to="/message" className="navElement">
+              Message
+            </Link>
+            <Link to="/connect" className="navElement">
+              Se connecter
+            </Link>
+            <Link to="/help" className="navElement">
+              Aide
+            </Link>
+            <Link to="/become-agent" className="navElement">
+              Devenez agent!
+            </Link>
+            <Link to="/login" className="navElement">
+              <AccountCircleIcon fontSize="large"/>
+            </Link>
           </Toolbar>
         </AppBar>
       </div>
@@ -46,4 +61,3 @@ function Nav() {
 }
 
 export default Nav;
-
