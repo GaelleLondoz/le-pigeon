bab0ae463bc2b425ce7a2d89bb2563b3df4c57df
Boris Ahishakiye <borisahishakiye@Boriss-MBP.lan>
2020-03-24 17:30:40 +0100

    signin material ui

--- front/src/App.js
+++ front/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import Places from './components/Places';
+import Signin from './components/signin'
  
 class App extends Component {
 
@@ -7,7 +8,7 @@ class App extends Component {
     return (
       <div className="app-viewport">
         <div className="container">
-          
+          <Signin />
           <Places/>
         </div>
       </div>
