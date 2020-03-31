8590bce0631d235bf3f21705c31e604a9c4191db
triumph75@gmail.com <triumph75@gmail.com>
2020-03-31 21:29:44 +0200

    Login management with authentication

--- front/src/components/services/API.js
+++ front/src/components/services/API.js
@@ -1,6 +1,27 @@
 import axios from "axios";
 
-export default axios.create({
-    baseURL: process.env.API_URL,
-    responseType: "json"
-});
+axios.defaults.baseURL = process.env.REACT_APP_API_URL;
+
+axios.interceptors.request.use(
+    request => {
+        console.log(request);
+        // Edit request config
+        return request;
+    },
+    error => {
+        console.log(error);
+        return Promise.reject(error);
+    }
+);
+
+axios.interceptors.response.use(
+    response => {
+        console.log(response);
+        // Edit response config
+        return response;
+    },
+    error => {
+        console.log(error);
+        return Promise.reject(error);
+    }
+);
