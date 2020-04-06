c8e80207c7d310b2c709a168f97cf650f74cb635
Naïm <nassna85@hotmail.fr>
2020-04-06 15:25:22 +0200

    Add Section Agent Avatar in Homepage

--- front/src/pages/Home.js
+++ front/src/pages/Home.js
@@ -2,12 +2,14 @@ import React from "react";
 import Places from "../components/Places";
 import Header from "./homepage/Header";
 import Plan from "./homepage/Plan";
+import AgentAvatar from "./homepage/AgentAvatar";
 
 const Home = () => {
   return (
     <>
       <Header />
       <Plan />
+      <AgentAvatar />
     </>
   );
 };
