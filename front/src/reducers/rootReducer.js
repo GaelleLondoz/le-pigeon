68c00cb0201f401dfa9c5ab6ff315ac833cf2b49
GaelleLondoz-BE <gaelle.londoz@decathlon.com>
2020-04-07 19:21:05 +0200

    set cookies

--- front/src/reducers/rootReducer.js
+++ front/src/reducers/rootReducer.js
@@ -1,4 +1,6 @@
-import { Cookies } from "react-cookie";
+import {Cookies} from "react-cookie";
+
+const cookies = new Cookies();
 
 const initState = {
     auth: null
@@ -8,9 +10,11 @@ const initState = {
 const rootReducer = (state = initState, action) => {
     if (action.type === "SET_AUTH") {
         console.log(action);
+        console.log(Cookies)
+        cookies.set("auth_token", action.payload.token, { path: '/' })
         return {
             auth: action.payload,
-            token: action.payload.token
+            token: action.payload.token,
         };
     }
     return state;
