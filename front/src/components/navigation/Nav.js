f62b6f13f92775c0d732ddd372e71821a54f2b30
Julien H <nordinleo@yahoo.fr>
2020-04-28 16:09:37 +0200

    with agent button

--- front/src/components/navigation/Nav.js
+++ front/src/components/navigation/Nav.js
@@ -260,6 +260,14 @@ let Nav = ({ history }) => {
               <HelpIcon className="icon" />
             </Link>
             <button className="navButton">Devenez agent!</button>
+=======
+            <Link to="/help" className="linkHelp">
+              <HelpIcon className="icon" />
+            </Link>
+            <Link to="/become-agent" className="navButton">
+              <p>Devenez agent!</p>
+            </Link>
+>>>>>>> with agent button
             <IconButton
               edge="end"
               aria-label="account of current user"
