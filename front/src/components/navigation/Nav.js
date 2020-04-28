65fc9fbbc4df58c5d95bef4e896e456f9aa29aef
Julien H <nordinleo@yahoo.fr>
2020-04-28 10:40:22 +0200

    new file navMobile

--- front/src/components/navigation/Nav.js
+++ front/src/components/navigation/Nav.js
@@ -4,7 +4,7 @@ import Avatar from "@material-ui/core/Avatar";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 import authAPI from "../services/authAPI";
-
+import renderMobileMenu from "./NavMobile";
 // Material-ui
 import { makeStyles } from "@material-ui/core/styles";
 import AppBar from "@material-ui/core/AppBar";
@@ -157,68 +157,7 @@ let Nav = ({ history }) => {
   );
 
   // menu mobile //////
-
-  const mobileMenuId = "primary-search-account-menu-mobile";
-  const renderMobileMenu = (
-    <RootRef rootRef={domRef}>
-      <Menu
-      anchorEl={mobileMoreAnchorEl}
-      anchorOrigin={{ vertical: "top", horizontal: "right" }}
-      id={mobileMenuId}
-      keepMounted
-      transformOrigin={{ vertical: "top", horizontal: "right" }}
-      open={isMobileMenuOpen}
-      onClose={handleMobileMenuClose}
-    >
-      <MenuItem>
-        <IconButton aria-label="show 4 new mails" color="inherit">
-          <Badge badgeContent={4} color="secondary">
-            <MailIcon />
-          </Badge>
-        </IconButton>
-        <p>Messages</p>
-      </MenuItem>
-      <MenuItem>
-        <IconButton aria-label="se connecter" color="inherit">
-          <PowerIcon />
-        </IconButton>
-        <p>Se connecter</p>
-      </MenuItem>
-      <MenuItem>
-        <IconButton aria-label="aide" color="inherit">
-          <HelpIcon />
-        </IconButton>
-        <p>Aide</p>
-      </MenuItem>
-      <MenuItem>
-        <IconButton aria-label="devenir agent" color="inherit">
-          <AgentIcon />
-        </IconButton>
-        <p>Devenir Agent</p>
-      </MenuItem>
-      <MenuItem onClick={handleProfileMenuOpen}>
-        <IconButton
-          aria-label="account of current user"
-          aria-controls="primary-search-account-menu"
-          aria-haspopup="true"
-          color="inherit"
-        >
-          <AccountCircle />
-        </IconButton>
-        <p>Profile</p>
-      </MenuItem>
-    </Menu>
-    </RootRef>
-  );
-
-  const { isAuthenticated } = useContext(AuthContext);
-
-  const [currentUser, setCurrentUser] = useState({
-    id: "",
-    firstName: "",
-    lastName: "",
-    avatar: "",
-  });
+    <renderMobileMenu />
 
   const fetchUser = async () => {
     try {
