a7289f8c8a1e24c1dbf3e355b8984444fc0c4b0e
triumph75@gmail.com <triumph75@gmail.com>
2020-04-30 22:48:43 +0200

    Sign In && Sign up modals

--- front/src/config.js
+++ front/src/config.js
@@ -5,3 +5,5 @@ export const AUTH_URL = API_URL + "/login";
 export const USER_URL = API_URL + "/me";
 
 export const LOGOUT_URL = API_URL + "/logout";
+
+export const USERS_URL = API_URL + "/users";
