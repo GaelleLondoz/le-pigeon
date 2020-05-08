083d25a1977851b33b18b73818f1bf956ef28ff1
Naïm <nassna85@hotmail.fr>
2020-05-08 20:50:32 +0200

    Add TabProfileAgent for link Profile in Profile Agent

---
+++ front/src/pages/profileAgent/Account.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Account = () => {
+  return (
+    <div>
+      <h1>Page Account in profile agent tab</h1>
+    </div>
+  );
+};
+
+export default Account;
--- front/src/pages/profileAgent/Profile.js
+++ front/src/pages/profileAgent/Profile.js
@@ -1,9 +1,11 @@
 import React from "react";
+import TabProfileAgent from "../../components/agent/TabProfileAgent";
 
 const Profile = () => {
   return (
     <div>
       <h1>Page Profile</h1>
+      <TabProfileAgent />
     </div>
   );
 };
