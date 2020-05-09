7b5ec89057c2dd7530dc43141ae04fd07ab215b2
Naïm <nassna85@hotmail.fr>
2020-05-10 01:19:01 +0200

    Display page evaluation profile agent with rating, lastName and avatar

--- front/src/pages/profileAgent/Evaluation.js
+++ front/src/pages/profileAgent/Evaluation.js
@@ -1,10 +1,78 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Container, Typography, Avatar, Grid, Box } from "@material-ui/core";
+import Rating from "@material-ui/lab/Rating";
+import UsersAPI from "../../components/services/userAPI";
 
 const Evaluation = () => {
+  const url = window.location.href;
+  const id = +url.substring(url.lastIndexOf("/") + 1);
+
+  const [agent, setAgent] = useState({
+    User: {
+      firstName: "",
+      lastName: "",
+      avatar: "",
+    },
+  });
+  const [avgRatings, setAvgRatings] = useState({
+    avgRatings: "",
+    countComments: "",
+  });
+
+  const fetchAvgRatings = async (id) => {
+    try {
+      const data = await UsersAPI.getAvgRatings(id);
+      const { avgRatings, countComments } = data[0];
+      setAvgRatings({ avgRatings, countComments });
+    } catch (error) {
+      console.log(error.response);
+    }
+  };
+
+  const fetchAgent = async (id) => {
+    try {
+      const data = await UsersAPI.getProfileAgent(id);
+      const { User } = data;
+      setAgent({ User });
+    } catch (error) {
+      console.log(error.response);
+    }
+  };
+
+  useEffect(() => {
+    fetchAgent(id);
+  }, [id]);
+
+  useEffect(() => {
+    fetchAvgRatings(id);
+  }, [id]);
+
   return (
-    <div>
-      <h1>Page Profile Evaluation</h1>
-    </div>
+    <section className="profile-agent-evaluation">
+      <Container>
+        <div className="profile-agent-evaluation-content">
+          <Grid container direction="column" justify="center">
+            <Grid item xs={12}>
+              <Typography variant="h4">
+                Evaluation de {agent.User.lastName}
+              </Typography>
+              <Typography component="p">
+                Actuellement, votre évaluation est calculée sur{" "}
+                {avgRatings.countComments} commentaire(s).
+              </Typography>
+              <div className="profile-agent-evaluation-info">
+                <Avatar alt="Remy Sharp" src={agent.User.avatar} />
+                <Rating
+                  name="read-only"
+                  value={+avgRatings.avgRatings}
+                  readOnly
+                />
+              </div>
+            </Grid>
+          </Grid>
+        </div>
+      </Container>
+    </section>
   );
 };
 
