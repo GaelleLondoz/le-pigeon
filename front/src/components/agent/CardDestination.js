1e5c680939ebd26501b7d1faf1d3f6eecda9458d
Naïm <nassna85@hotmail.fr>
2020-05-10 19:43:50 +0200

    Design card destination

--- front/src/components/agent/CardDestination.js
+++ front/src/components/agent/CardDestination.js
@@ -1,4 +1,6 @@
 import React from "react";
+import LocationCityIcon from "@material-ui/icons/LocationCity";
+import LocationOnIcon from "@material-ui/icons/LocationOn";
 import { Card, CardContent, Typography } from "@material-ui/core";
 import { formatDate } from "../../helpers/formatDate";
 
@@ -13,18 +15,29 @@ const CardDestination = ({ destination }) => {
         >
           {destination.Destination.name}
         </Typography>
-        <Typography variant="h5" component="h2">
-          {destination.Destination.Continent.name}
-        </Typography>
-        <Typography className="" color="textSecondary">
-          {destination.Destination.Country.name}
-        </Typography>
-        <Typography variant="body2" component="p">
-          {destination.remarks}
-        </Typography>
-        <Typography component="small">
-          Parti le {formatDate(destination.date)}
-        </Typography>
+        <div className="card-destination-info">
+          <LocationOnIcon />
+          <Typography variant="h5" component="h2">
+            {destination.Destination.Continent.name}
+          </Typography>
+        </div>
+        <div className="card-destination-info">
+          <LocationCityIcon />
+          <Typography
+            className="card-destination-country"
+            color="textSecondary"
+          >
+            {destination.Destination.Country.name}
+          </Typography>
+        </div>
+        <div className="card-destination-desc">
+          <Typography variant="body2" component="p">
+            {destination.remarks}
+          </Typography>
+          <Typography component="small">
+            Parti le {formatDate(destination.date)}
+          </Typography>
+        </div>
       </CardContent>
     </Card>
   );
