142a5bbbf84a36f36fc3fa651f14fda27f3d8a1a
Julien H <nordinleo@yahoo.fr>
2020-05-13 13:05:36 +0200

    working ternaire

--- front/src/components/navigation/NavPrivate.js
+++ front/src/components/navigation/NavPrivate.js
@@ -1,6 +1,5 @@
-import React, { Component } from "react";
-import { BrowserRouter, Link } from "react-router-dom";
-
+import React from "react";
+import { BrowserRouter, Link, useLocation } from "react-router-dom";
 // sass
 import "../../assets/sass/_nav_private.scss";
 
@@ -8,37 +7,44 @@ import "../../assets/sass/_nav_private.scss";
 
 
 
-class OnMatchedUrl extends Component {
-  constructor(props) {
-    super(props);
-  }//
+// class OnMatchedUrl extends Component {
+//   constructor(props) {
+//     super(props);
+//   }//
 
-  componentDidMount(props) {
-    this.AddClass(props)
-  }
+//   componentDidMount(props) {
+//     this.AddClass(props)
+//   }
 
-  AddClass(props) {
-    console.log('preclass')
-    if(window.location.pathname == this.props.pathname) {
-      console.log('class')
-      $(this.props.className).addClass("MatchedUrl")
-    } else {
-      $(this.props.className).removeClass("MatchedUrl")
-    }
-  }
+//   AddClass(props) {
+//     console.log('preclass')
+//     if(window.location.pathname == this.props.pathname) {
+//       console.log('class')
+//       $(this.props.className).addClass("MatchedUrl")
+//     } else {
+//       $(this.props.className).removeClass("MatchedUrl")
+//     }
+//   }
 
-}
+// }
+// function MatchUrl() {
+//   if (location.pathname === this.pathname) {
+//     className="MatchedUrl"
+//   } else {
+//     className=""
+
+//   }
+// }
 
 
 function NavPrivate(props) {
-  const test666 = console.log("grey grey grey")
-  const test777 = console.log("grey2 grey2 grey2")
+  const location = useLocation()
   return (
     <BrowserRouter>
       <div>
         <nav>
               <Link to="/profil">Profil</Link>
-              <Link to="/message" >MessagesSSSSS</Link>
+              <Link to="/message"  className={location.pathname === "/message" ? "MatchedUrl" : ""} >Messages</Link>
               <Link to="/agenda" >Agenda</Link>
               <Link to="/evaluation">Evaluations</Link>
               <Link to="/travel">Voyages</Link>
