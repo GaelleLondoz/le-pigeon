d9e10eabb2c367920ca60b1831ab98868e4db0c9
Naïm <nassna85@hotmail.fr>
2020-05-13 02:44:03 +0200

    Add button new destination in section travel profile agent

--- front/src/pages/profileAgent/Travel.js
+++ front/src/pages/profileAgent/Travel.js
@@ -1,9 +1,22 @@
 import React, { useState, useEffect } from "react";
-import { Container, Grid, Typography } from "@material-ui/core";
+import { Container, Grid, Typography, Button } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
 import UserDestinationsAPI from "../../components/services/userDestinationsAPI";
 import CardDestination from "../../components/agent/CardDestination";
 
+const useStyles = makeStyles((theme) => ({
+  buttonAddDestination: {
+    backgroundColor: "green",
+    color: "#fff",
+    "&:hover": {
+      backgroundColor: "green",
+      opacity: 0.8,
+    },
+  },
+}));
+
 const Travel = () => {
+  const classes = useStyles();
   const url = window.location.href;
   const id = +url.substring(url.lastIndexOf("/") + 1);
 
@@ -22,7 +35,7 @@ const Travel = () => {
     fetchDestinations(id);
   }, [id]);
 
-  console.log(destinations);
+  //console.log(destinations);
 
   return (
     <section className="profile-agent-destinations">
@@ -32,6 +45,9 @@ const Travel = () => {
           Vous avez effectué actuellement <strong>{destinations.length}</strong>{" "}
           voyage(s)
         </p>
+        <Button size="large" className={classes.buttonAddDestination}>
+          Nouvelle destination
+        </Button>
         <Grid container>
           {destinations.map((destination) => (
             <Grid key={destination.id} item m={4} xs={12} md={4}>
