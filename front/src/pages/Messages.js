6156a57b6ef085188206daefa2a1f19ea4085a7e
Julien H <nordinleo@yahoo.fr>
2020-05-16 09:27:36 +0200

    working nav again 2

--- front/src/pages/Messages.js
+++ front/src/pages/Messages.js
@@ -1,12 +1,10 @@
 import React from "react";
-import NavPrivate from "../components/navigation/NavPrivate"
 import Footer from "../components/Footer";
 
 
 function Messages() {
     return (
         <div>
-            <NavPrivate />
             <Footer />
         </div>
     )
