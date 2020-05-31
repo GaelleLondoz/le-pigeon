47152cd8108b3dac4238fd486dfcfcb6774a39fb
Naïm <nassna85@hotmail.fr>
2020-05-31 18:51:39 +0200

    Add a component Paginator and display this in messages profile agent

--- front/src/pages/profileAgent/Message.js
+++ front/src/pages/profileAgent/Message.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Container, Typography } from "@material-ui/core";
 import MessageAPI from "../../components/services/messageAPI";
 import CardMessage from "../../components/agent/CardMessage";
+import Paginator from "../../components/Pagination";
 
 const Message = () => {
   const url = window.location.href;
@@ -9,6 +10,9 @@ const Message = () => {
 
   const [messages, setMessages] = useState([]);
   const [notMessages, setNotMessages] = useState("");
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const ITEMS_PER_PAGE = 5;
 
   const fetchMessages = async (id) => {
     try {
@@ -22,6 +26,18 @@ const Message = () => {
     }
   };
 
+  //gestion du changement de page
+  const handlePaginationChange = (e, page) => {
+    setCurrentPage(page);
+    //window.scrollTo(0, 0);
+  };
+
+  const paginatedMessages = Paginator.getData(
+    messages,
+    currentPage,
+    ITEMS_PER_PAGE
+  );
+
   useEffect(() => {
     fetchMessages(id);
   }, [id]);
@@ -32,12 +48,21 @@ const Message = () => {
       <Container>
         <Typography variant="h5">Liste de vos messages</Typography>
         <p>Vous avez actuellement {messages.length} message(s)</p>
+        <p style={{ fontWeight: "bold" }}>Page {currentPage}</p>
         {notMessages ? (
           <p>{notMessages}</p>
         ) : (
-          messages.map((message) => (
+          paginatedMessages.map((message) => (
             <CardMessage key={message.id} message={message} />
           ))
+        )}
+        {messages.length > 5 && (
+          <Paginator
+            length={messages.length}
+            page={currentPage}
+            onPageChanged={handlePaginationChange}
+            itemsByPage={ITEMS_PER_PAGE}
+          />
         )}
       </Container>
     </section>
