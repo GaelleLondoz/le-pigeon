c26bb96804f24c0d1dfefaecebd666bb2dda9c7e
Naïm <nassna85@hotmail.fr>
2020-06-14 14:53:32 +0200

    Header account user design

--- front/src/pages/profileUser/UserAccount.js
+++ front/src/pages/profileUser/UserAccount.js
@@ -1,10 +1,46 @@
 import React from "react";
+import {
+  Container,
+  Avatar,
+  List,
+  ListItem,
+  ListItemIcon,
+  Grid,
+  ListItemText,
+  Typography,
+  ListItemAvatar,
+} from "@material-ui/core";
+import InboxIcon from "@material-ui/icons/Inbox";
+import FolderIcon from "@material-ui/icons/Folder";
 
 const UserAccount = () => {
   return (
-    <div>
-      <h1>Page account User</h1>
-    </div>
+    <>
+      <section id="profile-user-account-header">
+        <Container>
+          <div className="container-header">
+            <Avatar
+              alt="Remy Sharp"
+              src="https://randomuser.me/api/portraits/men/35.jpg"
+            />
+            <div className="container-info">
+              <div className="profile-user-account-info">
+                <InboxIcon />
+                <Typography variant="p">Jean Dubosq</Typography>
+              </div>
+              <div className="profile-user-account-info">
+                <InboxIcon />
+                <Typography variant="p">Jean Dubosq</Typography>
+              </div>
+              <div className="profile-user-account-info">
+                <InboxIcon />
+                <Typography variant="p">Jean Dubosq</Typography>
+              </div>
+            </div>
+          </div>
+        </Container>
+      </section>
+    </>
   );
 };
 
