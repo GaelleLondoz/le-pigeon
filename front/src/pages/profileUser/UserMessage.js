05b0c8896ec85fa4ea97d25de42e1368f66f7d5f
Naïm <nassna85@hotmail.fr>
2020-06-15 15:49:26 +0200

    Add component pagination in list messages profile user

--- front/src/pages/profileUser/UserMessage.js
+++ front/src/pages/profileUser/UserMessage.js
@@ -2,12 +2,16 @@ import React, { useState, useEffect } from "react";
 import { Container, Typography } from "@material-ui/core";
 import MessageAPI from "../../components/services/messageAPI";
 import CardMessage from "../../components/agent/CardMessage";
+import Paginator from "../../components/Pagination";
 
 const UserMessage = ({ match }) => {
   const { id } = match.params;
 
   const [messages, setMessages] = useState([]);
   const [notMessages, setNotMessages] = useState("");
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const ITEMS_PER_PAGE = 5;
 
   const fetchMessages = async (id) => {
     try {
@@ -20,6 +24,17 @@ const UserMessage = ({ match }) => {
       console.log(error.response);
     }
   };
+
+  const handlePaginationChange = (e, page) => {
+    setCurrentPage(page);
+    //window.scrollTo(0, 0);
+  };
+
+  const paginatedMessages = Paginator.getData(
+    messages,
+    currentPage,
+    ITEMS_PER_PAGE
+  );
 
   useEffect(() => {
     fetchMessages(id);
@@ -37,9 +52,17 @@ const UserMessage = ({ match }) => {
         {notMessages ? (
           <p>{notMessages}</p>
         ) : (
-          messages.map((message) => (
+          paginatedMessages.map((message) => (
             <CardMessage key={message.id} message={message} />
           ))
+        )}
+        {messages.length > 5 && (
+          <Paginator
+            length={messages.length}
+            page={currentPage}
+            onPageChanged={handlePaginationChange}
+            itemsByPage={ITEMS_PER_PAGE}
+          />
         )}
       </Container>
     </section>
