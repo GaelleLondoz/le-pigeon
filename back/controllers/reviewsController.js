d0e176c6963ebc9734bc7e0a6b1aae7f85887225
Naïm <nassna85@hotmail.fr>
2020-06-28 13:21:05 +0200

    Section evaluation agent change in api condition if not comments => add .length and in front display message

--- back/controllers/reviewsController.js
+++ back/controllers/reviewsController.js
@@ -8,7 +8,8 @@ const index = async (req, res) => {
     .catch((e) => res.status(500).send(e)); */
   try {
     const ratings = await db.sequelize.query(
-      "SELECT Reviews.id,Reviews.comment,Reviews.rating,Reviews.status,Users.firstName,Users.lastName FROM Reviews JOIN Users ON Users.id = Reviews.authorID OR Users.id = Reviews.agentID", { type: sequelize.QueryTypes.SELECT }
+      "SELECT Reviews.id,Reviews.comment,Reviews.rating,Reviews.status,Users.firstName,Users.lastName FROM Reviews JOIN Users ON Users.id = Reviews.authorID OR Users.id = Reviews.agentID",
+      { type: sequelize.QueryTypes.SELECT }
     );
     if (!ratings) {
       return res.status(403).json({ msg: "Bookings Not Found" });
@@ -22,32 +23,33 @@ const index = async (req, res) => {
 
 const create = (req, res) => {
   const newReview = req.body.review;
-  newReview.status = "PENDING"
-
+  newReview.status = "PENDING";
 
   return Review.create(newReview)
-    .then(review => {
-      res.status(200).send(review)
+    .then((review) => {
+      res.status(200).send(review);
     })
-    .catch(e => res.status(500).send(e));
+    .catch((e) => res.status(500).send(e));
 };
 
 const findOne = (req, res) => {
   const id = req.params.id;
   return Review.findByPk(id)
-    .then(review => res.status(200).send(review))
-    .catch(e => res.status(500).send(e));
-}
+    .then((review) => res.status(200).send(review))
+    .catch((e) => res.status(500).send(e));
+};
 
 const update = (req, res) => {
   const id = req.params.id;
   const statusData = {
-      status: req.params.status,
+    status: req.params.status,
   };
 
   Review.update(statusData, {
-      where: { id: id },
-  }).then(review => res.status(200).send(review)).catch(e => res.status(500).send(e));
+    where: { id: id },
+  })
+    .then((review) => res.status(200).send(review))
+    .catch((e) => res.status(500).send(e));
 };
 
 const destroy = (req, res) => {
@@ -56,7 +58,7 @@ const destroy = (req, res) => {
     where: { id: id },
   })
 
-    .then(() => res.status(200).json({ message: 'review deleted' }))
+    .then(() => res.status(200).json({ message: "review deleted" }))
     .catch((e) => res.status(500).send(e));
 };
 
@@ -92,13 +94,16 @@ const getAllCommentsReviewByAgent = async (req, res) => {
       // added status: "PUBLISHED"
       where: { agentID: id, status: "PUBLISHED" },
       attributes: ["id", "comment", "rating", "authorID", "status"],
-      include: [{
-        model: User,
-        as: "author",
-        attributes: ["firstName", "lastName", "avatar"],
-      },],
+      include: [
+        {
+          model: User,
+          as: "author",
+          attributes: ["firstName", "lastName", "avatar"],
+        },
+      ],
     });
-    if (!comments) {
+    console.log(comments);
+    if (!comments.length) {
       return res.status(404).json({ msg: "Comments Not Found" });
     }
     return res.status(200).json(comments);
@@ -112,18 +117,19 @@ const reviewsByAgent = (req, res) => {
   const id = req.params.agentID;
   Review.findAll({
     where: { agentID: id },
-    include: [{
-      model: User,
-      as: "author"
-    }]
+    include: [
+      {
+        model: User,
+        as: "author",
+      },
+    ],
   })
-    .then(review => {
-      console.log(review)
-      res.status(200).send(review)
+    .then((review) => {
+      console.log(review);
+      res.status(200).send(review);
     })
-    .catch(e => res.status(500).send(e));
-
-}
+    .catch((e) => res.status(500).send(e));
+};
 
 module.exports = {
   index,
