f7e6bbca904313851d419919fe0e257b5605a177
GaelleLondoz-BE <gaelle.londoz@decathlon.com>
2020-07-16 20:54:29 +0200

    wip

--- front/src/helpers/compareCurrentDate.js
+++ front/src/helpers/compareCurrentDate.js
@@ -13,13 +13,21 @@ export const compareDateForUpdateBooking = (date) => {
 
 export const displayCallButton = (date) => {
   const DATE = Date.now();
-  // return new Date(date).getTime() === new Date(DATE).getTime();
+  // return new Date(date) === new Date(DATE);
 
-  /*
-    var today = new Date();
-    var Christmas = new Date("2012-12-25");
-    var diffMs = (Christmas - today); // milliseconds between now & Christmas
-    var diffMins = Math.round(((diffMs % 86400000) % 3600000) / 60000); // minutes
-  */
-  return true;
+  // ajouter heure, mois, année, minutes
+  let today = new Date();
+  let booking = new Date(date);
+  // let diffMs = (booking - today);
+  // let diffMins = Math.round(((diffMs % 86400000) % 3600000) / 60000);
+  // console.log({ diffMins })
+
+
+  // var tenMinutes = 10 * 60 * 1000; /* ms */
+  // console.log({ calcul: today - booking })
+  // console.log({ diff: ((today - booking) < tenMinutes) })
+
+
+
+  // return diffMins <= 10 ? true : false
 };
